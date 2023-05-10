7ec99c09270686d0e73ce002451ba0bc3485fa9e
Abdulloh <abdullohbahromjonv@gmail.com>
2023-05-10 11:43:54 +0500

    Initial Commit

---
+++ Google Translate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Google Translate
+//
+//  Created by Abdulloh on 10/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Google Translate/Google_TranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_TranslateApp.swift
+//  Google Translate
+//
+//  Created by Abdulloh on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Google_TranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
